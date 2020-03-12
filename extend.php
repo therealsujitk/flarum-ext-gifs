a22b61c7f65f616f5ddda0b4a9dcc51b03f55535
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2020-03-12 14:29:14 +0530

    Fixed: SettingsModal not showing up

--- extend.php
+++ extend.php
@@ -16,5 +16,7 @@ use Flarum\Extend;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js'),
     new Extend\Locales(__DIR__ . '/resources/locale')
 ];
