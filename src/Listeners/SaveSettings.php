f6959cf595e7b8e2695751c8ed6b7a8a72cd11cf
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-04-24 22:56:18 +0200

    Resolve SettingsRepositoryInterface as late as possible This gives the opportunity to other extensions to change its implementation Also added the missing check for ForumSerializer so that the value only gets added to the forum payload and not every other serialized payload

--- src/Listeners/SaveSettings.php
+++ src/Listeners/SaveSettings.php
@@ -2,23 +2,24 @@
 
 namespace Therealsujitk\GIFs\Listeners;
 
-use Flarum\Api\Serializer\UserSerializer;
+use Flarum\Api\Serializer\ForumSerializer;
 use Flarum\Api\Event\Serializing;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Events\Dispatcher;
 
-class SaveSettings {
-	protected $settings;
+class SaveSettings
+{
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(Serializing::class, [$this, 'addAttributes']);
+    }
 
-	public function __construct(SettingsRepositoryInterface $settings) {
-		$this->settings = $settings;
-	}
+    public function addAttributes(Serializing $event)
+    {
+        if ($event->isSerializer(ForumSerializer::class)) {
+            $settings = app(SettingsRepositoryInterface::class);
 
-	public function subscribe(Dispatcher $events) {
-		$events->listen(Serializing::class, [$this, 'addAttributes']);
-	}
-
-	public function addAttributes(Serializing $event) {
-		$event->attributes['therealsujitk-gifs.giphy_api_key'] = $this->settings->get('therealsujitk-gifs.giphy_api_key');
-	}
+            $event->attributes['therealsujitk-gifs.giphy_api_key'] = $settings->get('therealsujitk-gifs.giphy_api_key');
+        }
+    }
 }
