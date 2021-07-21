2d18ac45dc90723d4636e648d07def82954741a8
Sujit Kumar <me@therealsuji.tk>
2021-07-21 19:03:17 +0530

    Moved styles to a separate file

--- extend.php
+++ extend.php
@@ -17,7 +17,9 @@ use Flarum\Settings\SettingsRepositoryInterface;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js'),
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
+        
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
     new Extend\Locales(__DIR__ . '/resources/locale'),
--- js/src/forum/components/SearchGIFModal.js
+++ js/src/forum/components/SearchGIFModal.js
@@ -1,15 +1,24 @@
 import Modal from 'flarum/common/components/Modal';
+import Button from 'flarum/common/components/Button';
+import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
 
-const giphyLimit = '40';
-var query = '';
-var lastResult = 0;
+const giphyLimit = 20;
+var textArea, giphyAPI, query, offset;
 
-function getGiphyURL(textarea, giphyAPI) {
-    let url;
-    if (query === '')
-        url = 'https://api.giphy.com/v1/gifs/trending?api_key=' + giphyAPI + '&limit=' + giphyLimit + '&offset=' + lastResult.toString(10);
-    else
-        url =
+function getGiphy() {
+    var url = (() => {
+        if (query === '') {
+            return (
+                'https://api.giphy.com/v1/gifs/trending?api_key=' +
+                giphyAPI +
+                '&limit=' +
+                giphyLimit +
+                '&offset=' +
+                offset
+            );
+        }
+
+        return (
             'https://api.giphy.com/v1/gifs/search?api_key=' +
             giphyAPI +
             '&q=' +
@@ -17,67 +26,52 @@ function getGiphyURL(textarea, giphyAPI) {
             '&limit=' +
             giphyLimit +
             '&offset=' +
-            lastResult.toString(10);
+            offset
+        );
+    })();
+
+    var container = document.getElementById('therealsujitk-gifs-container');
+    var columns = container.getElementsByClassName('therealsujitk-gifs-column');
+    var loadingIndicator = container.getElementsByClassName('LoadingIndicator-container')[0];
+    var endMessage = container.getElementsByClassName('therealsujitk-gifs-end')[0];
+
+    if (offset == 0) {
+        for (var i = 0; i < columns.length; ++i) {
+            columns[i].innerHTML = '';
+        }
+
+        endMessage.style.display = 'none';
+        loadingIndicator.style.display = 'flex';
+    }
 
     fetch(url)
         .then((response) => response.json())
         .then((content) => {
             if (typeof content.data === 'undefined') {
-                console.error('Sorry, there was something wrong with the GIFs API Key.');
+                console.error('Sorry, there was something wrong with the Giphy API Key.');
                 return;
             }
 
-            let resultsLeft = document.getElementById('LeftResults');
-            let resultsRight = document.getElementById('RightResults');
-            let flag = 0;
-
-            for (var i = 0; i < parseInt(giphyLimit); i += 2) {
-                let imgL = document.createElement('img');
-                let imgR = document.createElement('img');
-
-                document.getElementsByClassName('temp-text')[0].textContent = '';
-                document.getElementById('LoadMore').style.visibility = 'hidden';
-                document.getElementById('LoadMoreSpan').textContent = app.translator.trans('therealsujitk.forum.gifs.loadmore');
-
-                if (typeof content.data[0] === 'undefined') {
-                    document.getElementsByClassName('temp-text')[0].textContent = app.translator.trans('therealsujitk.forum.gifs.noresults');
-                    ++flag;
-                    break;
-                }
+            for (var i = 0; i < giphyLimit; ++i) {
+                var gif = document.createElement('img');
 
                 if (typeof content.data[i] === 'undefined') {
-                    ++flag;
+                    loadingIndicator.style.display = 'none';
+                    endMessage.style.display = 'inline-block';
+
                     break;
                 }
-                imgL.src = content.data[i].images.downsized.url;
-                imgL.alt = content.data[i].title;
-                imgL.style =
-                    'min-width: 97.5%; width: 97.5%; border-radius: 5px; margin: 1.25%; margin-left: 0%; margin-right: 0.3125%; vertical-align: top; cursor: pointer;';
-                resultsLeft.insertAdjacentElement('beforeend', imgL);
-                imgL.onclick = () => {
-                    let embed = '![Giphy - ' + imgL.alt + ']' + '(' + imgL.src + ')';
-                    app.modal.close();
-                    textarea.insertAtCursor(embed);
-                };
 
-                if (typeof content.data[i + 1] === 'undefined') {
-                    ++flag;
-                    break;
-                }
-                imgR.src = content.data[i + 1].images.downsized.url;
-                imgR.alt = content.data[i + 1].title;
-                imgR.style =
-                    'min-width: 97.5%; width: 97.5%; border-radius: 5px; margin: 1.25%; margin-left: 0.3125%; margin-right: 0%; vertical-align: top; cursor: pointer;';
-                resultsRight.insertAdjacentElement('beforeend', imgR);
-                imgR.onclick = () => {
-                    let embed = '![Giphy - ' + imgR.alt + ']' + '(' + imgR.src + ')';
+                gif.src = content.data[i].images.downsized.url;
+                gif.alt = content.data[i].title;
+                gif.addEventListener('click', (e) => {
+                    var embed = '![Giphy - ' + e.target.alt + ']' + '(' + e.target.src + ')';
+                    textArea.insertAtCursor(embed);
                     app.modal.close();
-                    textarea.insertAtCursor(embed);
-                };
-            }
+                });
 
-            if (flag === 0) {
-                document.getElementById('LoadMore').style.visibility = 'visible';
+                // Currently all screen sizes have 2 columns
+                columns[i % 2].insertAdjacentElement('beforeend', gif);
             }
         });
 }
@@ -96,117 +90,105 @@ export default class SearchGIFModal extends Modal {
             '.Modal-body',
             {
                 oncreate: () => {
-                    document.getElementsByClassName('temp-text')[0].textContent = app.translator.trans('therealsujitk.forum.gifs.loading');
-                    const textarea = this.attrs.textArea;
-                    const giphyAPI = app.forum.attribute('therealsujitk-gifs.giphy_api_key');
-                    query = document.getElementById('GIFSearchBar').value.trim();
-                    getGiphyURL(textarea, giphyAPI);
-                },
+                    textArea = this.attrs.textArea;
+                    giphyAPI = app.forum.attribute('therealsujitk-gifs.giphy_api_key');
+                    query = '';
+                    offset = 0;
+
+                    getGiphy();
+                }
             },
-            m('span[style = position: absolute; left: 50%; top: 200px; transform: translate(-50%, -50%);]', {
-                id: 'flarum-loading',
-                class: 'temp-text',
-            }),
-            m('div', [
-                m('table[style = vertical-align: top; width: 100%;]', [
-                    m('td[style = width: 100%;]', [
-                        m(
-                            'form[style = margin-right: 1.25%;]',
-                            {
-                                class: 'Search-input',
-                                onsubmit: () => {
-                                    return false;
-                                },
-                                onkeyup: (e) => {
-                                    if (e.keyCode === 13) {
-                                        lastResult = 0;
-                                        document.getElementById('LeftResults').innerHTML = '';
-                                        document.getElementById('RightResults').innerHTML = '';
-                                        document.getElementById('LeftResults').scrollTop = 0;
-                                        document.getElementsByClassName('temp-text')[0].textContent = app.translator.trans(
-                                            'therealsujitk.forum.gifs.loading'
-                                        );
-                                        document.getElementById('LoadMore').style.visibility = 'hidden';
-                                        const textarea = this.attrs.textArea;
-                                        const giphyAPI = app.forum.attribute('therealsujitk-gifs.giphy_api_key');
-                                        query = document.getElementById('GIFSearchBar').value.trim();
-                                        getGiphyURL(textarea, giphyAPI);
-                                    }
-                                },
-                            },
-                            [
-                                m('input[style = width: 100%;]', {
-                                    id: 'GIFSearchBar',
-                                    class: 'FormControl',
-                                    type: 'search',
-                                    autocomplete: 'off',
-                                    placeholder: app.translator.trans('therealsujitk.forum.gifs.searchbar'),
-                                }),
-                            ]
-                        ),
-                    ]),
-                    m('td', [
-                        m(
-                            'button[style = margin-left: 1.25%;]',
-                            {
-                                class: 'Button Button--primary hasIcon',
-                                itemclassname: 'App-primaryControl',
-                                type: 'button',
-                                title: app.translator.trans('therealsujitk.forum.gifs.search'),
-                                onclick: () => {
-                                    lastResult = 0;
-                                    document.getElementById('LeftResults').innerHTML = '';
-                                    document.getElementById('RightResults').innerHTML = '';
-                                    document.getElementById('LeftResults').scrollTop = 0;
-                                    document.getElementsByClassName('temp-text')[0].textContent = app.translator.trans(
-                                        'therealsujitk.forum.gifs.loading'
-                                    );
-                                    document.getElementById('LoadMore').style.visibility = 'hidden';
-                                    const textarea = this.attrs.textArea;
-                                    const giphyAPI = app.forum.attribute('therealsujitk-gifs.giphy_api_key');
-                                    query = document.getElementById('GIFSearchBar').value.trim();
-                                    getGiphyURL(textarea, giphyAPI);
-                                },
-                            },
-                            [m('span', { class: 'Button-label' }, app.translator.trans('therealsujitk.forum.gifs.search'))]
-                        ),
-                    ]),
-                ]),
-            ]),
-            m('div[style = margin-top: 10px; margin-bottom: 10px; min-height: 45vh; height: 45vh; overflow: auto;]', [
-                m('table', { width: '100%' }, [
-                    m('td', {
-                        id: 'LeftResults',
-                        width: '50%',
-                    }),
-                    m('td', {
-                        id: 'RightResults',
-                        width: '50%',
-                    }),
-                ]),
-                m(
-                    'button[style = position: relative; left: 50%; transform: translate(-50%, 0%); margin-top: 10px; visibility: hidden;]',
-                    {
-                        id: 'LoadMore',
-                        class: 'Button',
-                        type: 'button',
-                        title: app.translator.trans('therealsujitk.forum.gifs.loadmore'),
-                        onclick: () => {
-                            lastResult += parseInt(giphyLimit);
-                            document.getElementById('LoadMoreSpan').textContent = app.translator.trans('therealsujitk.forum.gifs.loading');
-                            const textarea = this.attrs.textArea;
-                            const giphyAPI = app.forum.attribute('therealsujitk-gifs.giphy_api_key');
-                            getGiphyURL(textarea, giphyAPI);
+            m(
+                'div',
+                {
+                    id: 'therealsujitk-gifs-search'
+                },
+                [
+                    m(
+                        'div',
+                        {
+                            className: 'Search-input'
                         },
-                    },
-                    [m('span', { id: 'LoadMoreSpan', class: 'Button-label' }, app.translator.trans('therealsujitk.forum.gifs.loadmore'))]
-                ),
-            ]),
-            m('div[style = padding-top: 10px; padding-bottom: 30px;]', [
-                m('img[style = float: right;]', {
-                    src: app.forum.attribute('baseUrl') + '/assets/extensions/therealsujitk-gifs/powered_by_giphy.svg',
-                }),
-            ])
+                        [
+                            m('input', {
+                                className: 'FormControl',
+                                placeholder: app.translator.trans('therealsujitk.forum.gifs.searchGiphy'),
+                                onkeydown: (e) => {
+                                    if (e.key === 'Enter') {
+                                        e.preventDefault(); // To prevent the page from reloading
+
+                                        query = document
+                                            .getElementById('therealsujitk-gifs-search')
+                                            .getElementsByTagName('input')[0].value;
+                                        offset = 0;
+
+                                        getGiphy();
+                                    }
+                                }
+                            })
+                        ]
+                    ),
+                    Button.component(
+                        {
+                            className: 'Button Button--primary hasIcon',
+                            onclick: () => {
+                                query = document
+                                    .getElementById('therealsujitk-gifs-search')
+                                    .getElementsByTagName('input')[0].value;
+                                offset = 0;
+
+                                getGiphy();
+                            }
+                        },
+                        app.translator.trans('therealsujitk.forum.gifs.search')
+                    )
+                ]
+            ),
+            m(
+                'div',
+                {
+                    id: 'therealsujitk-gifs-container',
+                    oncreate: () => {
+                        var container = document.getElementById('therealsujitk-gifs-container');
+
+                        container.addEventListener('scroll', () => {
+                            if (container.scrollTop === container.scrollHeight - container.offsetHeight) {
+                                offset += giphyLimit;
+                                getGiphy();
+                            }
+                        });
+                    }
+                },
+                [
+                    m('div', {
+                        className: 'therealsujitk-gifs-column'
+                    }),
+                    m('div', {
+                        className: 'therealsujitk-gifs-column'
+                    }),
+                    LoadingIndicator.component(),
+                    m(
+                        'span',
+                        {
+                            className: 'therealsujitk-gifs-end'
+                        },
+                        app.translator.trans('therealsujitk.forum.gifs.end')
+                    )
+                ]
+            ),
+            m(
+                'div',
+                {
+                    id: 'therealsujitk-gifs-footer'
+                },
+                [
+                    m('img', {
+                        src:
+                            app.forum.attribute('baseUrl') +
+                            '/assets/extensions/therealsujitk-gifs/powered_by_giphy.svg'
+                    })
+                ]
+            )
         );
     }
 }
